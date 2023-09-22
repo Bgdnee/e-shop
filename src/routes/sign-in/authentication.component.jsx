2f0794240b24c5dcad5bc2c86ad0dd16f31f0180
Bgd <bgdnee@gmail.com>
2023-09-22 07:16:06 +0300

    created authethication page component and sign in form

---
+++ src/routes/sign-in/authentication.component.jsx
@@ -0,0 +1,25 @@
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-in-form.component";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(user);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignIn;
